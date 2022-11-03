030675b4d5c5baea4547895769c97086b64dadcd
hirano <rhi691508@gmail.com>
2022-11-03 15:23:18 +0900

    Get BlogTitle and BlogContent#17

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreatePost.css";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+
+  const createPost = () => {
+    console.log(title);
+    console.log(postText);
+  };
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入" />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder="投稿内容を記入"></textarea>
-          <button className="postButton">投稿する</button>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={(e) => setTitle(e.target.value)}
+          ></textarea>
+          <button className="postButton" onClick={createPost}>
+            投稿する
+          </button>
         </div>
       </div>
     </div>
