5bb15b127f5c75576e5b0ca3ff3fe889d6634346
hirano <rhi691508@gmail.com>
2022-11-05 06:28:32 +0900

    Create HomePage templete#23

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきたいと思います。よろしくおねがいします。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@HiraCode</h3>
+        <button>削除</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
