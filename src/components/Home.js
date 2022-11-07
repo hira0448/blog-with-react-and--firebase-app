8ba58dc5453d171a46314fab13413746966d7871
hirano <rhi691508@gmail.com>
2022-11-07 23:45:16 +0900

    Get data from Firestore#17

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,40 @@
-import React, { useEffect } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import "./Home.css";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
-    const getposts = async () => {
+    const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
-    getposts();
+    getPosts();
   }, []);
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-
-        <div className="postTextContainer">
-          今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきたいと思います。よろしくおねがいします。
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@HiraCode</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            console.log({post});
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
